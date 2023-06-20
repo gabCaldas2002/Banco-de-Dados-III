0bfb9ebb54819733b523dba6050bad7b22a8e90d
gabCaldas2002 <gabriel.caldas20112002@gmail.com>
2023-06-20 09:36:38 -0300

    teste

---
+++ meusgastos-caldas/src/main/java/com/example/meusgastoscaldas/domain/service/UsuarioService.java
@@ -0,0 +1,51 @@
+package com.example.meusgastoscaldas.domain.service;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.meusgastoscaldas.domain.dto.usuario.UsuarioRequestDTO;
+import com.example.meusgastoscaldas.domain.dto.usuario.UsuarioResponseDTO;
+import com.example.meusgastoscaldas.domain.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService implements ICRUDService<UsuarioRequestDTO, UsuarioResponseDTO>{
+
+@Autowired
+private UsuarioRepository usuarioRepository;
+@Autowired
+private ModelMapper mapper;
+
+    @Override
+    public List<UsuarioResponseDTO> obterTodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obterTodos'");
+    }
+
+    @Override
+    public UsuarioResponseDTO obterPorId(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obterPorId'");
+    }
+
+    @Override
+    public UsuarioResponseDTO cadastrar(Long id, UsuarioRequestDTO dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cadastrar'");
+    }
+
+    @Override
+    public UsuarioResponseDTO atualizar(Long id, UsuarioRequestDTO dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+    }
+
+    @Override
+    public void deletar(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletar'");
+    }
+    
+}
