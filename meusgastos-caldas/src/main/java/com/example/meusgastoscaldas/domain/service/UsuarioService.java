88f9356ebab4bb39d5bbaf9fa2d748124da205e9
gabCaldas2002 <gabriel.caldas20112002@gmail.com>
2023-06-27 09:02:59 -0300

    bd 27/06

--- meusgastos-caldas/src/main/java/com/example/meusgastoscaldas/domain/service/UsuarioService.java
+++ meusgastos-caldas/src/main/java/com/example/meusgastoscaldas/domain/service/UsuarioService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
-import org.modelmapper.internal.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
