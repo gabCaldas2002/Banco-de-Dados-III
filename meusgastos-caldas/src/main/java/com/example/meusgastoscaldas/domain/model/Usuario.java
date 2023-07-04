22612649f0b469eeed4eb341228da0ef4f69a041
gabCaldas2002 <gabriel.caldas20112002@gmail.com>
2023-07-04 09:34:54 -0300

    bd 04/07

--- meusgastos-caldas/src/main/java/com/example/meusgastoscaldas/domain/model/Usuario.java
+++ meusgastos-caldas/src/main/java/com/example/meusgastoscaldas/domain/model/Usuario.java
@@ -4,8 +4,8 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -15,7 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
 @Entity
-public class Usuario /*implements UserDetails*/{
+public class Usuario implements UserDetails{
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column (name="idUsuario")
@@ -81,7 +81,7 @@ public class Usuario /*implements UserDetails*/{
     public void setTitulos(List<Titulo> titulos) {
         this.titulos = titulos;
     }
-    /*
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
@@ -110,5 +110,5 @@ public class Usuario /*implements UserDetails*/{
     public boolean isEnabled() {
         return true;
     }
-*/
+    
 }
