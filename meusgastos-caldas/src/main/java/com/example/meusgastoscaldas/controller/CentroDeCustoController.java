db0dce524ff4df0d539ed30e29d4e550ad21b102
gabCaldas2002 <gabriel.caldas20112002@gmail.com>
2023-07-18 09:46:56 -0300

    BD 18/07

--- meusgastos-caldas/src/main/java/com/example/meusgastoscaldas/controller/CentroDeCustoController.java
+++ meusgastos-caldas/src/main/java/com/example/meusgastoscaldas/controller/CentroDeCustoController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +32,7 @@ public class CentroDeCustoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<List<CentroDeCustoResponseDTO>> obterPorId(@PathVariable Long id){
+    public ResponseEntity<CentroDeCustoResponseDTO> obterPorId(@PathVariable Long id){
         return ResponseEntity.ok(centroDeCustoService.obterPorId(id));
     }
 
@@ -38,6 +40,18 @@ public class CentroDeCustoController {
     public ResponseEntity<CentroDeCustoResponseDTO> cadastrar(@RequestBody CentroDeCustoRequestDTO dto){
         CentroDeCustoResponseDTO responseDTO = centroDeCustoService.cadastrar(dto);
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CentroDeCustoResponseDTO> atualizar(@PathVariable Long id, @RequestBody CentroDeCustoResponseDTO dto){
+        CentroDeCustoResponseDTO responseDTO = centroDeCustoService.atualizar(id, dto);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+        
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletar(@PathVariable Long id){
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT)
     }
     
 }
